f41d7ed305241cadddc0844d77531d1aefd79c56
Frank Snyder <fs031448@broadcom.net>
2025-07-23 14:10:12 -0400

    pausing the startup for capture.

--- lab-update.sh
+++ lab-update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 . /home/holuser/.bashrc
-ansible-playbook /vpodrepo/2026-labs/2670/lab-startup/lab-update.yml | tee -a /lmchol/hol/labstartup.log >> /home/holuser/hol/labstartup.log 2>&1
+# ansible-playbook /vpodrepo/2026-labs/2670/lab-startup/lab-update.yml | tee -a /lmchol/hol/labstartup.log >> /home/holuser/hol/labstartup.log 2>&1
 
-ansible-playbook /vpodrepo/2026-labs/2670/mgmt-seg-pso/vDefend_DFW_Configuration.yml | tee -a /lmchol/hol/labstartup.log >> /home/holuser/hol/labstartup.log 2>&1
+# ansible-playbook /vpodrepo/2026-labs/2670/mgmt-seg-pso/vDefend_DFW_Configuration.yml | tee -a /lmchol/hol/labstartup.log >> /home/holuser/hol/labstartup.log 2>&1
