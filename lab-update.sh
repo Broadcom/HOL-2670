5ecfac348d59f2c9fb7bc53b8c11e28e93243b4e
Frank Snyder <fs031448@broadcom.net>
2025-08-15 15:52:00 -0400

    Enabled GEO and MGMT Domain Scripts

--- lab-update.sh
+++ lab-update.sh
@@ -6,6 +6,6 @@
 
 # ansible-playbook /vpodrepo/2026-labs/2670/lab-startup/lab-update.yml | tee -a /lmchol/hol/labstartup.log >> /home/holuser/hol/labstartup.log 2>&1
 
-# ansible-playbook /vpodrepo/2026-labs/2670/mgmt-seg-pso/vDefend_DFW_Configuration.yml | tee -a /lmchol/hol/labstartup.log >> /home/holuser/hol/labstartup.log 2>&1
+ansible-playbook /vpodrepo/2026-labs/2670/mgmt-seg-pso/vDefend_DFW_Configuration.yml | tee -a /lmchol/hol/labstartup.log >> /home/holuser/hol/labstartup.log 2>&1
 
-# /bin/bash /vpodrepo/2026-labs/2670/lab-startup/clear-geo.sh | tee -a /lmchol/hol/labstartup.log >> /home/holuser/hol/labstartup.log 2>&1
+/bin/bash /vpodrepo/2026-labs/2670/lab-startup/clear-geo.sh | tee -a /lmchol/hol/labstartup.log >> /home/holuser/hol/labstartup.log 2>&1
