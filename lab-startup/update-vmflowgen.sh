94709b3d5ffcd473333248c0360ba84f1bed5706
Frank Snyder <fs031448@broadcom.net>
2025-08-18 23:42:18 -0400

    updated spelling

--- lab-startup/update-vmflowgen.sh
+++ lab-startup/update-vmflowgen.sh
@@ -8,7 +8,7 @@ vPodPW=$(</home/holuser/creds.txt)
 
 #Copy over the updated vdefend firewall demo topology
 
-sshpass -p $vPodPW ssh root@vmflowgen.site-a.vcf.lab  'rm /opt/vmware/vmflowgen/topologies/defendfirewalldemo.yaml'
-sshpass -p $vPodPW scp /vpodrepo/2026-labs/2670/vmflowgen/vdefendfirewalldemo.yaml root@vmflowgen.site-a.vcf.lab:/opt/vmware/vmflowgen/topologies/defendfirewalldemo.yaml
+sshpass -p $vPodPW ssh root@vmflowgen.site-a.vcf.lab  'rm /opt/vmware/vmflowgen/topologies/vdefendfirewalldemo.yaml'
+sshpass -p $vPodPW scp /vpodrepo/2026-labs/2670/vmflowgen/vdefendfirewalldemo.yaml root@vmflowgen.site-a.vcf.lab:/opt/vmware/vmflowgen/topologies/vdefendfirewalldemo.yaml
 
 #Download and copy over the update libray.yaml
