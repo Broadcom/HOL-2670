021ad34b6378a5316f0215322f04aefbd608cb30
Frank Snyder <fs031448@broadcom.net>
2025-08-19 11:49:44 -0400

    updated with external download link

--- lab-startup/update-vmflowgen.sh
+++ lab-startup/update-vmflowgen.sh
@@ -12,10 +12,11 @@ sshpass -p $vPodPW ssh root@vmflowgen.site-a.vcf.lab  'rm /opt/vmware/vmflowgen/
 sshpass -p $vPodPW scp /vpodrepo/2026-labs/2670/vmflowgen/vdefendfirewalldemo.yaml root@vmflowgen.site-a.vcf.lab:/opt/vmware/vmflowgen/topologies/vdefendfirewalldemo.yaml
 
 #Download and copy over the update libray.yaml
-#gpg --pinentry-mode=loopback --passphrase 'VMware123!VMware123!' --output /vpodrepo/2026-labs/2670/vmflowgen/library.yaml.tar.gz --decrypt /vpodrepo/2026-labs/2670/vmflowgen/library.yaml.tar.gz.gpg
-#tar -xvzf /vpodrepo/2026-labs/2670/vmflowgen/library.yaml.tar.gz -C /vpodrepo/2026-labs/2670/vmflowgen
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1gTZJhDqQLg7Q6A6Oe3_JveTysgvQh3JL' -O /home/holuser/library.yaml.tar.gz.gpg
+gpg --pinentry-mode=loopback --passphrase 'VMware123!VMware123!' --output /home/holuser/library.yaml.tar.gz --decrypt /home/holuser/library.yaml.tar.gz.gpg
+tar -xvzf /home/holuser/vmflowgen/library.yaml.tar.gz -C /home/holuser/vmflowgen
 
-#Copy the library file to the flowgen VM
-#sshpass -p $vPodPW ssh root@vmflowgen.site-a.vcf.lab  'rm /opt/vmware/vmflowgen/library.yaml'
-#sshpass -p $vPodPW scp /vpodrepo/2026-labs/2670/vmflowgen/library.yaml root@vmflowgen.site-a.vcf.lab:/opt/vmware/vmflowgen/library.yaml
-##sshpass -p $vPodPW ssh root@vmflowgen.site-a.vcf.lab  'systemctl restart vmflowgen'
+Copy the library file to the flowgen VM
+sshpass -p $vPodPW ssh root@vmflowgen.site-a.vcf.lab  'rm /opt/vmware/vmflowgen/library.yaml'
+sshpass -p $vPodPW scp /home/holuser/library.yaml root@vmflowgen.site-a.vcf.lab:/opt/vmware/vmflowgen/library.yaml
+sshpass -p $vPodPW ssh root@vmflowgen.site-a.vcf.lab  'systemctl restart vmflowgen'
