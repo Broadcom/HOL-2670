e8c2d122049ae019800b0763df22bcee1a209005
Frank Snyder <fs031448@broadcom.net>
2025-08-19 11:25:40 -0400

    add code to update the library.yaml file.

--- lab-startup/update-vmflowgen.sh
+++ lab-startup/update-vmflowgen.sh
@@ -12,3 +12,10 @@ sshpass -p $vPodPW ssh root@vmflowgen.site-a.vcf.lab  'rm /opt/vmware/vmflowgen/
 sshpass -p $vPodPW scp /vpodrepo/2026-labs/2670/vmflowgen/vdefendfirewalldemo.yaml root@vmflowgen.site-a.vcf.lab:/opt/vmware/vmflowgen/topologies/vdefendfirewalldemo.yaml
 
 #Download and copy over the update libray.yaml
+#gpg --pinentry-mode=loopback --passphrase 'VMware123!VMware123!' --output /vpodrepo/2026-labs/2670/vmflowgen/library.yaml.tar.gz --decrypt /vpodrepo/2026-labs/2670/vmflowgen/library.yaml.tar.gz.gpg
+#tar -xvzf /vpodrepo/2026-labs/2670/vmflowgen/library.yaml.tar.gz -C /vpodrepo/2026-labs/2670/vmflowgen
+
+#Copy the library file to the flowgen VM
+#sshpass -p $vPodPW ssh root@vmflowgen.site-a.vcf.lab  'rm /opt/vmware/vmflowgen/library.yaml'
+#sshpass -p $vPodPW scp /vpodrepo/2026-labs/2670/vmflowgen/library.yaml root@vmflowgen.site-a.vcf.lab:/opt/vmware/vmflowgen/library.yaml
+##sshpass -p $vPodPW ssh root@vmflowgen.site-a.vcf.lab  'systemctl restart vmflowgen'
