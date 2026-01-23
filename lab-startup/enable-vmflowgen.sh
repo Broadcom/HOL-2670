aa549b662af5cc292a193e0693ab9abb238b7744
Frank Snyder <fs031448@broadcom.net>
2026-01-23 09:07:49 -0500

    Updated Flowgen startup scripts

--- lab-startup/enable-vmflowgen.sh
+++ lab-startup/enable-vmflowgen.sh
@@ -8,14 +8,26 @@
 vPodUID="admin"
 vPodPW=$(</home/holuser/creds.txt) 
 
+# ATP Demo
+curl -s -k -X POST -u ${vPodUID}:${vPodPW} https://vmflowgen.site-a.vcf.lab/apply?config_name=atp
+
+# Baremetal Demo
+curl -s -k -X POST -u ${vPodUID}:${vPodPW} https://vmflowgen.site-a.vcf.lab/apply?config_name=baremetal
+
+# IDPS Demo
+curl -s -k -X POST -u ${vPodUID}:${vPodPW} https://vmflowgen.site-a.vcf.lab/apply?config_name=idpsdemo
+
 # Malware Demo
 curl -s -k -X POST -u ${vPodUID}:${vPodPW} https://vmflowgen.site-a.vcf.lab/apply?config_name=malwarepreventiondemo
 
-# IPS Demo
-curl -s -k -X POST -u ${vPodUID}:${vPodPW} https://vmflowgen.site-a.vcf.lab/apply?config_name=idpsdemo
-
-# Intelligence Demo
-curl -s -k -X POST -u ${vPodUID}:${vPodPW} https://vmflowgen.site-a.vcf.lab/apply?config_name=intelligence-hol
+# NDR External Sensor Demo
+curl -s -k -X POST -u ${vPodUID}:${vPodPW} https://vmflowgen.site-a.vcf.lab/apply?config_name=ndr-external-sensor
 
 # vDefend Firewall demo
 curl -s -k -X POST -u ${vPodUID}:${vPodPW} https://vmflowgen.site-a.vcf.lab/apply?config_name=vdefendfirewalldemo
+
+# Security Journey Demo
+curl -s -k -X POST -u ${vPodUID}:${vPodPW} https://vmflowgen.site-a.vcf.lab/apply?config_name=security-journey
+
+# Intelligence Demo no longer used
+# curl -s -k -X POST -u ${vPodUID}:${vPodPW} https://vmflowgen.site-a.vcf.lab/apply?config_name=intelligence-hol
