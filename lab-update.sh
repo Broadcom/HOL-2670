6801308c9fd9fdf01aa50b66cd80e151601953f6
Frank Snyder <fs031448@broadcom.net>
2026-03-18 12:38:05 -0700

    corrected boot script

--- lab-update.sh
+++ lab-update.sh
@@ -18,7 +18,7 @@
 /bin/bash /vpodrepo/2026-labs/2670/lab-startup/enable-vmflowgen.sh | tee -a /lmchol/hol/labstartup.log >> /home/holuser/hol/labstartup.log 2>&1
 
 #Update SSP Groups for W01. M01 will remain the same
-ansible-playbook /vpodrepo/2026-labs/2670/tag-ssp-vms.yml | tee -a /lmchol/hol/labstartup.log >> /home/holuser/hol/labstartup.log 2>&1
+ansible-playbook /vpodrepo/2026-labs/2670/lab-startup/tag-ssp-vms.yml | tee -a /lmchol/hol/labstartup.log >> /home/holuser/hol/labstartup.log 2>&1
 
 #Deploy the DFW policy for the management domain
 #ansible-playbook /vpodrepo/2026-labs/2670/mgmt-seg-pso/vDefend_DFW_Configuration.yml | tee -a /lmchol/hol/labstartup.log >> /home/holuser/hol/labstartup.log 2>&1
