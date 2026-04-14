b63afac1877e29a5ad00aa4cf89e2325f2ad2f5c
Frank Snyder <fs031448@broadcom.net>
2026-04-14 13:29:13 -0400

    Updated Startup Config

--- lab-startup/renew-ssp-pw.sh
+++ lab-startup/renew-ssp-pw.sh
@@ -13,50 +13,50 @@ tmpPW+="!"
 vPodTOKEN=$(echo -n "${vPodUID}:${vPodPW}" | base64)
 tmpTOKEN=$(echo -n "${vPodUID}:${tmpPW}" | base64)
 
-echo $tmpPW
-echo $vPodTOKEN
-echo $tmpTOKEN
+echo "Temporary Password for SSP and SSPI is: $tmpPW"
 
 echo "Setting TMP password on SSP"
 # Step 1- Change to temporary pass:
-curl -k --location "https://ssp.site-a.vcf.lab/ssp/auth/change-password" \
---header "Content-Type: application/json" \
---header "Authorization: Basic ${vPodTOKEN}" \
---data "{
-    'username': ${vPodUID},
-    'old_password': ${vPodPW},
-    'password': ${tmpPW}
-}"
-
-echo "Setting Lab password on SSP"
-# Step 2- change back to same password with extension of 180days:
- curl -k --location "https://ssp.site-a.vcf.lab/ssp/auth/change-password" \
---header "Content-Type: application/json" \
---header "Authorization: Basic ${tmpTOKEN}" \
---data "{
-    'username': ${vPodUID},
-    'old_password': ${tmpPW},
-    'password': ${vPodPW}
+curl -k -w "\n%{http_code}\n" --location 'https://ssp.site-a.vcf.lab/ssp/auth/change-password' \
+-H "Content-Type: application/json" \
+-u "$vPodUID:$vPodPW" \
+-d "{
+    \"username\": \"$vPodUID\",
+    \"old_password\": \"$vPodPW\",
+    \"password\": \"$tmpPW\"
 }"
 
 echo "Setting TMP password on SSPi"
 # Step 3 - Change SSPI to have a temporary Password
-curl -k "https://ssp-i.site-a.vcf.lab/sspi/operations/accounts" \
---header "Content-Type: application/json" \
---header "Authorization: Basic ${tmpTOKEN}" \
---data "{
-    'username': ${vPodUID},
-    'old_password': ${vPodPW},
-    'password': ${tmpPW}
+curl -k -w "\n%{http_code}\n" --location --request POST 'https://ssp-i.site-a.vcf.lab/sspi/operations/accounts' \
+-H "Content-Type: application/json" \
+-u "$vPodUID:$vPodPW" \
+-d "{
+    \"account_name\": \"$vPodUID\",
+    \"current_password\": \"$vPodPW\",
+    \"new_password\": \"$tmpPW\"
+}"
+
+sleep 2m
+
+echo "Setting Lab password on SSP"
+# Step 2- change back to same password with extension of 180days:
+ curl -k -w "\n%{http_code}\n" --location 'https://ssp.site-a.vcf.lab/ssp/auth/change-password' \
+-H "Content-Type: application/json" \
+-u "$vPodUID:$tmpPW" \
+-d "{
+    \"username\": \"$vPodUID\",
+    \"old_password\": \"$tmpPW\",
+    \"password\": \"$vPodPW\"
 }"
 
 echo "Setting Lab password on SSPi"
 # Step 4 - Chage SSPI to have the Lab Password
-curl -k "https://ssp-i.site-a.vcf.lab/sspi/operations/accounts" \
---header "Content-Type: application/json" \
---header "Authorization: Basic ${tmpTOKEN}" \
---data "{
-    'username': ${vPodUID},
-    'old_password': ${tmpPW},
-    'password': ${vPodPW}
+curl -k -w "\n%{http_code}\n" --location --request POST 'https://ssp-i.site-a.vcf.lab/sspi/operations/accounts' \
+-H "Content-Type: application/json" \
+-u "$vPodUID:$tmpPW" \
+-d "{
+    \"account_name\": \"$vPodUID\",
+    \"current_password\": \"$tmpPW\",
+    \"new_password\": \"$vPodPW\"
 }"
