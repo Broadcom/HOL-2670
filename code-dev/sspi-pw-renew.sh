13125c6e1983f73a586ea201bfb1401cdb9da169
Frank Snyder <fs031448@broadcom.net>
2026-04-15 16:13:56 -0400

    trying expect

--- code-dev/sspi-pw-renew.sh
+++ code-dev/sspi-pw-renew.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#!/usr/bin/expect -f
 
 #Every 180 the admin password for SSPI will expire and will need to be renewed.
 #This script will reset the timer for the expired SSPI admin passwored in Production HOL Evironment.
@@ -10,10 +11,24 @@ tmpPW+="!"
 
 echo $tmpPW
 
-sshpass -p $vPodPW ssh -t sysadmin@ssp-i.site-a.vcf.lab "echo $tmpPW | echo $tmpPW | echo $vPodPW | sudo -S /opt/vmware/vsx-operator/bin/reset_user_cred.py -u 'admin'"
+#sshpass -p $vPodPW ssh -t sysadmin@ssp-i.site-a.vcf.lab "echo $tmpPW | echo $tmpPW | echo $vPodPW | sudo -S /opt/vmware/vsx-operator/bin/reset_user_cred.py -u 'admin'"
+spawn sshpass -p $vPodPW ssh -t sysadmin@ssp-i.site-a.vcf.lab "echo $vPodPW | sudo -S /opt/vmware/vsx-operator/bin/reset_user_cred.py -u 'admin'"
+expect "New password:"
+respond "$tmpPW"
+expect "Retype new password:"
+respond "$tmpPW
+interact
+
 
 echo "\nPassword set to the temporary Password listed above\n"
 
 sleep 1m
 
-sshpass -p $vPodPW ssh -t sysadmin@ssp-i.site-a.vcf.lab "echo $vPodPW | echo $vPodPW | echo $vPodPW | sudo -S /opt/vmware/vsx-operator/bin/reset_user_cred.py -u 'admin'"
+#sshpass -p $vPodPW ssh -t sysadmin@ssp-i.site-a.vcf.lab "echo $vPodPW | echo $vPodPW | echo $vPodPW | sudo -S /opt/vmware/vsx-operator/bin/reset_user_cred.py -u 'admin'"
+
+spawn sshpass -p $vPodPW ssh -t sysadmin@ssp-i.site-a.vcf.lab "echo $vPodPW | sudo -S /opt/vmware/vsx-operator/bin/reset_user_cred.py -u 'admin'"
+expect "New password:"
+respond "$vPodPW"
+expect "Retype new password:"
+respond "$vPodPW
+interact
