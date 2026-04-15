d2bc77991110be67451fce9a8c0e47be77d08559
Frank Snyder <fs031448@broadcom.net>
2026-04-15 16:00:34 -0400

    added code that will add a temp pw

--- code-dev/sspi-pw-renew.sh
+++ code-dev/sspi-pw-renew.sh
@@ -5,5 +5,15 @@
 #It will execute on every vPod Boot and will result in the password being new
 
 vPodPW=$(</home/holuser/creds.txt) 
+tmpPW=$(tr -dc 'A-Za-z0-9!#$@' < /dev/urandom | head -c 16)
+tmpPW+="!"
 
-sshpass -p $vPodPW ssh -t sysadmin@ssp-i.site-a.vcf.lab "echo $vPodPW | sudo -S /opt/vmware/vsx-operator/bin/reset_user_cred.py -u 'admin'"
+echo $tmpPW
+
+sshpass -p $vPodPW ssh -t sysadmin@ssp-i.site-a.vcf.lab "echo $tmpPW | echo $tmpPW | echo $vPodPW | sudo -S /opt/vmware/vsx-operator/bin/reset_user_cred.py -u 'admin'"
+
+echo "\nPassword set to the temporary Password listed above\n"
+
+sleep 1m
+
+sshpass -p $vPodPW ssh -t sysadmin@ssp-i.site-a.vcf.lab "echo $vPodPW | echo $vPodPW | echo $vPodPW | sudo -S /opt/vmware/vsx-operator/bin/reset_user_cred.py -u 'admin'"
